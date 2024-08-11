0011cccb26ea5ffd6c4caa80fbf9e774b570440e
zfpxs <2436871821@qq.com>
2024-08-11 11:16:52 +0800

    Update providers.jsx

--- client/next-web/src/app/providers.jsx
+++ client/next-web/src/app/providers.jsx
@@ -6,9 +6,7 @@ import { useEffect } from 'react';
 
 
 export function Providers({ children }) {
-  useEffect(()=>{
-    document.documentElement.classList.remove('dark'); // 强制移除 dark 类
-  }, [])
+
   return (
     <NextUIProvider>
       <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
